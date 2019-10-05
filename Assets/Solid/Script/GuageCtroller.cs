c44b48e0b94c5f917de680a92344d08589bd03a1
SolidTooth <bluenote642@gmail.com>
2019-10-05 22:38:43 +0900

    게이지 작업 완료

---
+++ Assets/Solid/Script/GuageCtroller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GuageCtroller : MonoBehaviour
+{
+    [SerializeField]
+    private KeyStrokeInputController keyCtrl;
+    [SerializeField]
+    private float aniSpeed;
+    [SerializeField]
+    private Slider rightSlider;
+    [SerializeField]
+    private Slider leftSlider;
+    [SerializeField]
+    private Animator[] rightAnis;
+    [SerializeField]
+    private Animator[] leftAnis;
+    private void LateUpdate()
+    {
+        rightSlider.value = keyCtrl.RightSpeed;
+        leftSlider.value = keyCtrl.LeftSpeed;
+        for (int i = 0; i < rightAnis.Length; i++)
+        {
+            rightAnis[i].SetFloat("AddSpeed", aniSpeed * keyCtrl.RightSpeed);
+        }
+        for (int i = 0; i < leftAnis.Length; i++)
+        {
+            leftAnis[i].SetFloat("AddSpeed", aniSpeed * keyCtrl.LeftSpeed);
+        }
+    }
+}
