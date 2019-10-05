89bcc220f3f28d1270f40fbc02f29de39292b893
AcrylicShrimp <led789zxpp@naver.com>
2019-10-05 12:57:57 +0900

    Implemented impact system.

--- Assets/AShrimp/DoovooController.cs
+++ Assets/AShrimp/DoovooController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 [RequireComponent(typeof(PathReceiver), typeof(Rigidbody))]
 public class DoovooController : MonoBehaviour
 {
+    public enum ImpactDirection
+    {
+        Left,
+        Right
+    }
+
     [SerializeField]
     private float _BaseVectorSpeed;
     [SerializeField]
@@ -25,5 +31,24 @@ public class DoovooController : MonoBehaviour
 
         //Add some drag.
         this.sRigidbody.AddForce(Mathf.Min(0f, this._MaximumSpeed - Vector3.Dot(this.sRigidbody.velocity, this.sPathReceiver.PathDirection)) * this.sPathReceiver.PathDirection * this._BaseVectorSpeed, ForceMode.Acceleration);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.collider.CompareTag("Wall"))
+            return;
+
+        var sNormal = collision.GetContact(0).normal;
+
+        this.sRigidbody.AddForce(this.sRigidbody.velocity + 2f * sNormal, ForceMode.VelocityChange);
+
+        ImpactDirection sImpactDirection;
+
+        if (Vector3.Dot(sNormal, Vector3.right) < 0f)
+            sImpactDirection = ImpactDirection.Right;
+        else
+            sImpactDirection = ImpactDirection.Left;
+
+        Debug.Log(sImpactDirection);
     }
 }
